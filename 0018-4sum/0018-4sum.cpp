ff652961c8ac4b6e50f66682730af6abfd443ae1
shivam-pratap75 <shivam9193496575@gmail.com>
2025-10-02 13:26:47 +0530

    Time: 14 ms (91.65%), Space: 17.3 MB (83.16%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -14,18 +14,21 @@ public:
             if(j>i+1&& nums[j]==nums[j-1] ) continue;
             left=j+1;
             right=n-1;
+
+            long long res=(long long)target-nums[i]-nums[j];
             while(left<right){
-                 long long sum=(long long)nums[i]+nums[j]+nums[left]+nums[right];  
-              if(sum<target){ left++;
-              }else if(sum>target){
-                 right--;
-              }else{
-                result.push_back({nums[i],nums[j],nums[left],nums[right]});
+                long long sum=(long long)nums[left]+nums[right];
+             if(sum==res){
+                 result.push_back({nums[i],nums[j],nums[left],nums[right]});
                 while(left<right && nums[left]==nums[left+1])left++;
               while(left<right && nums[right]==nums[right-1])right--;
                right--;
                 left++;
-              }            
+             }else if(sum<res){ 
+                left++;
+              }else{
+                 right--;
+              }         
 
             }
         }
