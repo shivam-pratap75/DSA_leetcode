65bd28ae2bda342e98ecb4414e63d8167e4e9c47
shivam-pratap75 <shivam9193496575@gmail.com>
2025-10-22 02:05:51 +0530

    Time: 0 ms (100%), Space: 15.3 MB (32.85%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-
+        int left = 0;
+        int right = nums.size() - 1;
         
-        int left=0;
-        int right=nums.size()-1;
-       
-
-  while(left<=right){
-          
- int mid=(left+right)/2;
-if(nums[mid]==target){
-    return mid;
-}
-
-if(nums[left]<=nums[mid]){
-    if(nums[left]<=target && target < nums[mid]){
-        right=mid-1;
-    }else{
-        left=mid+1;
-    }
-}else{
-    if(nums[mid]<target && target<=nums[right]){
-        left=mid+1;
-    }else{
-        right=mid-1;
-    }
-}
-
-
+        while (left <= right) {
+            int mid = left + (right - left) / 2;  // Avoids overflow
+            
+            if (nums[mid] == target) {  // Fixed: compare nums[mid] with target
+                return mid;
+            }
+            
+            // Check which side is sorted
+            if (nums[left] <= nums[mid]) {  // Left side is sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;  // Target is in left sorted portion
+                } else {
+                    left = mid + 1;   // Target is in right portion
+                }
+            } else {  // Right side is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;   // Target is in right sorted portion
+                } else {
+                    right = mid - 1;  // Target is in left portion
+                }
+            }
         }
-
-
+        
         return -1;
     }
 };
