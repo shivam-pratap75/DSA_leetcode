e75c96b6884ef0629db3198869470a9b501d4dca
shivam-pratap75 <shivam9193496575@gmail.com>
2025-10-23 20:10:17 +0530

    Time: 951 ms (7%), Space: 64.5 MB (5.42%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,29 +2,34 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         if (s1.size() > s2.size()) return false;
+        
         unordered_map<char, int> values;
-        for (char c : s1) {
-            values[c]++;
-        }
+        for (char c : s1) values[c]++;
        
-        for (int i = 0; i < s2.size(); i++) {
-            if (values.find(s2[i]) != values.end()) {
+      
+        for (int i = 0; i <= s2.size() - s1.size(); i++) {
+            if (values.count(s2[i])) {
                 unordered_map<char, int> temp = values;
-                int j = i;
+                int usedChars = 0;
                 
-                while (j < s2.size()) {
-                    if (temp.find(s2[j]) != temp.end() && temp[s2[j]] > 0) {
-                        temp[s2[j]]--;
-                        if (temp[s2[j]] == 0) {
-                            temp.erase(s2[j]);
+                for (int j = i; j < s2.size() && usedChars < s1.size(); j++) {
+                    char c = s2[j];
+                    if (!temp.count(c) || temp[c] == 0) break;
+                    
+                    temp[c]--;
+                    usedChars++;
+                    
+                    if (usedChars == s1.size()) {
+                        bool allUsed = true;
+                        for (auto& [ch, cnt] : temp) {
+                            if (cnt != 0) {
+                                allUsed = false;
+                                break;
+                            }
                         }
-                        j++;
-                        if (temp.empty()) return true;
-                    } else {
-                        break;
+                        if (allUsed) return true;
                     }
                 }
-              
             }
         }
         return false;
