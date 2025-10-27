f3cedfa370aea266bb21642868fdbef917d82ef8
shivam-pratap75 <shivam9193496575@gmail.com>
2025-10-27 20:56:53 +0530

    Time: 0 ms (100%), Space: 71.7 MB (80.45%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,11 +5,9 @@ public:
         int max_sum=INT_MIN;
 
         for(int i=0;i<nums.size();i++){
-            curr_sum+=nums[i];
+            curr_sum=max(curr_sum+nums[i],nums[i]);
             max_sum=max(curr_sum,max_sum);
-          if(curr_sum<0){
-            curr_sum=0;
-          }
+          
         }
         return max_sum;
         
