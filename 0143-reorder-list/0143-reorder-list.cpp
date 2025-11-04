7b7458cfd04e9539f42fc1663ba5054230f2770a
shivam-pratap75 <shivam9193496575@gmail.com>
2025-11-04 17:44:06 +0530

    Time: 317 ms (5.05%), Space: 22.8 MB (57.84%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) return;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        ListNode* temp = head;
+
+        while(temp != NULL && temp->next != NULL) {
+            
+            ListNode* preserve = slow;
+            while(preserve->next != NULL && preserve->next->next != NULL){
+                preserve = preserve->next;
+            }
+            
+            
+            if(preserve->next == NULL) break;
+            
+            ListNode* lastNode = preserve->next;
+            preserve->next = NULL;
+            
+            ListNode* nextTemp = temp->next;
+            temp->next = lastNode;
+            lastNode->next = nextTemp;
+
+            temp = nextTemp;
+            
+            
+            if(temp == slow) break;
+        }
+    }
+};
