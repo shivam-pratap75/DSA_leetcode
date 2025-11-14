fa50e1aca8eab0cb54fcbdd46d3e72ce9fe7d26b
shivam-pratap75 <shivam9193496575@gmail.com>
2025-11-14 20:15:40 +0530

    Time: 58 ms (63.99%), Space: 14.3 MB (61.85%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,30 +1,53 @@
+// class Solution {
+// public:
+//     int operation(vector<int>& nums,int curr,int prev,vector<vector<int>>& dp){
+        
+//         if(curr==nums.size()){
+//             return 0;
+//         }
+//     if( dp[curr][prev+1]!=0){
+//         return dp[curr][prev+1];
+    
+//     }
+//         int include=0;
+//         if(prev==-1 || nums[curr]>nums[prev]){
+
+//             include=1+ operation(nums,curr+1,curr,dp);
+//         }
+
+//         int exclude=0+operation(nums,curr+1,prev,dp);
+
+//         return dp[curr][prev+1]=max(include,exclude);
+        
+//     }
+    
+//     int lengthOfLIS(vector<int>& nums) {
+//         vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,0));
+
+//         return operation(nums,0,-1,dp);
+        
+//     }
+// };
+
+
+// bottom up DP
 class Solution {
 public:
-    int operation(vector<int>& nums,int curr,int prev,vector<vector<int>>& dp){
-        
-        if(curr==nums.size()){
-            return 0;
-        }
-    if( dp[curr][prev+1]!=0){
-        return dp[curr][prev+1];
-    
-    }
-        int include=0;
-        if(prev==-1 || nums[curr]>nums[prev]){
-
-            include=1+ operation(nums,curr+1,curr,dp);
-        }
-
-        int exclude=0+operation(nums,curr+1,prev,dp);
-
-        return dp[curr][prev+1]=max(include,exclude);
-        
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,0));
-
-        return operation(nums,0,-1,dp);
+        int n = nums.size();
+        vector<int> dp(n, 1); 
         
+        int maxLIS = 1;
+        
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLIS = max(maxLIS, dp[i]);
+        }
+        
+        return maxLIS;
     }
 };
