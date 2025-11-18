f9ae51892b1f58729d469fa94f2067101cb158d8
shivam-pratap75 <shivam9193496575@gmail.com>
2025-11-18 15:11:45 +0530

    Time: 3 ms (3.39%), Space: 13 MB (72.06%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
@@ -13,11 +13,11 @@ public:
  int val=0;
  bool result=true;
  int i=0;
-       while(i<n-1){
+       while(i<n){
 
         if(bits[i]==1){
               val=1;
-              if(bits[i+1]==0 || bits[i+1]==1){
+              if(i<n && bits[i+1]==0 || bits[i+1]==1){
                 val=0;
                 result=false;
                 i++;
