b3f718396a281757fb99d48c1b11e3a005c96f9a
shivam-pratap75 <shivam9193496575@gmail.com>
2026-01-04 21:04:12 +0530

    Time: 14 ms (68.23%), Space: 16.2 MB (71.43%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,36 +1,70 @@
+// class Solution {
+// public:
+//     int sumFourDivisors(vector<int>& nums) {
+
+//    int result=0;
+
+//    for(int i=0;i<nums.size();i++){
+//     int curr=0;
+//     int flag=0;
+//     for(int j=2;j*j<=nums[i];j++){
+//         if(flag>2){
+//             break;
+//         }
+//         if(nums[i]%j==0){
+//            flag++;
+//            curr+=j;
+//         int a=nums[i]/j;
+//         if(nums[i]%a==0 && a!=j){
+//             flag++;
+//             curr+=a;
+//         }
+
+//         }
+//     }
+//     if(flag==2){
+//         result+=curr;
+//         result+=1+nums[i];
+//     }
+//    }
+
+
+
+// return result;
+        
+//     }
+// };
+
 class Solution {
 public:
     int sumFourDivisors(vector<int>& nums) {
 
-   int result=0;
+        int n=nums.size();
+        int result=0;
 
-   for(int i=0;i<nums.size();i++){
-    int curr=0;
-    int flag=0;
-    for(int j=2;j*j<=nums[i];j++){
-        if(flag>2){
-            break;
+        for(int i=0;i<n;i++){
+            int check=0;
+            int sum=0;
+
+            for(int j=2;j*j<=nums[i];j++){
+                if(nums[i]%j==0){
+                    check++;
+                    sum+=j;
+                    int b=nums[i]/j;
+                    if(nums[i]%b==0 && j!=b){
+                        check++;
+                        sum+=b;
+                    }
+                    
+                }
+                if(check>2)break;
+               
+            }
+            if(check==2){
+                result=result+1+nums[i]+sum;
+                
+            }
         }
-        if(nums[i]%j==0){
-           flag++;
-           curr+=j;
-        int a=nums[i]/j;
-        if(nums[i]%a==0 && a!=j){
-            flag++;
-            curr+=a;
-        }
-
-        }
-    }
-    if(flag==2){
-        result+=curr;
-        result+=1+nums[i];
-    }
-   }
-
-
-
-return result;
-        
+            return result;
     }
 };
