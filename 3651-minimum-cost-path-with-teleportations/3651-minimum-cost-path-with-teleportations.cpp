3da35d6d181a37ee13666a6d3452c58bb83782ce
shivam-pratap75 <shivam9193496575@gmail.com>
2026-01-28 14:45:35 +0530

    Time: 467 ms (77.95%), Space: 130.5 MB (82.68%) - LeetHub

--- 3651-minimum-cost-path-with-teleportations/3651-minimum-cost-path-with-teleportations.cpp
+++ 3651-minimum-cost-path-with-teleportations/3651-minimum-cost-path-with-teleportations.cpp
@@ -1,63 +1,50 @@
 class Solution {
 public:
     int minCost(vector<vector<int>>& grid, int k) {
-        int n = grid.size(), m = grid[0].size();
-
-        map<int, vector<pair<int,int>>> mp;
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                mp[grid[i][j]].push_back({i,j});
-
-        vector<vector<vector<int>>> dist(n,
-            vector<vector<int>>(m, vector<int>(k+1, INT_MAX)));
-
-        using T = tuple<int,int,int,int>;
-        priority_queue<T, vector<T>, greater<T>> pq;
-
-        dist[0][0][0] = 0;
-        pq.push({0,0,0,0});
-
-        // pointer per teleport layer
-        vector<map<int, vector<pair<int,int>>>::iterator> ptr(k+1, mp.begin());
-
-        while(!pq.empty()){
-            auto [cost,i,j,t] = pq.top();
-            pq.pop();
-
-            if(cost > dist[i][j][t]) continue;
-            if(i==n-1 && j==m-1) return cost;
-
-            // normal moves
-            if(i+1<n){
-                int nc = cost + grid[i+1][j];
-                if(nc < dist[i+1][j][t]){
-                    dist[i+1][j][t] = nc;
-                    pq.push({nc,i+1,j,t});
-                }
+        int m = grid.size(), n = grid[0].size();
+        vector<pair<int, int>> points;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                points.push_back({i, j});
             }
-            if(j+1<m){
-                int nc = cost + grid[i][j+1];
-                if(nc < dist[i][j+1][t]){
-                    dist[i][j+1][t] = nc;
-                    pq.push({nc,i,j+1,t});
+        }
+        sort(points.begin(), points.end(),
+             [&](const auto& p1, const auto& p2) -> bool {
+                 return grid[p1.first][p1.second] < grid[p2.first][p2.second];
+             });
+        vector<vector<int>> costs(m, vector<int>(n, INT_MAX));
+        for (int t = 0; t <= k; t++) {
+            int minCost = INT_MAX;
+            for (int i = 0, j = 0; i < points.size(); i++) {
+                minCost =
+                    min(minCost, costs[points[i].first][points[i].second]);
+                if (i + 1 < points.size() &&
+                    grid[points[i].first][points[i].second] ==
+                        grid[points[i + 1].first][points[i + 1].second]) {
+                    continue;
                 }
+                for (int r = j; r <= i; r++) {
+                    costs[points[r].first][points[r].second] = minCost;
+                }
+                j = i + 1;
             }
-
-            // teleport
-            if(t < k){
-                int val = grid[i][j];
-
-                while(ptr[t] != mp.end() && ptr[t]->first <= val){
-                    for(auto &[x,y] : ptr[t]->second){
-                        if(cost < dist[x][y][t+1]){
-                            dist[x][y][t+1] = cost;
-                            pq.push({cost,x,y,t+1});
-                        }
+            for (int i = m - 1; i >= 0; i--) {
+                for (int j = n - 1; j >= 0; j--) {
+                    if (i == m - 1 && j == n - 1) {
+                        costs[i][j] = 0;
+                        continue;
                     }
-                    ++ptr[t];
+                    if (i != m - 1) {
+                        costs[i][j] =
+                            min(costs[i][j], costs[i + 1][j] + grid[i + 1][j]);
+                    }
+                    if (j != n - 1) {
+                        costs[i][j] =
+                            min(costs[i][j], costs[i][j + 1] + grid[i][j + 1]);
+                    }
                 }
             }
         }
-        return -1;
+        return costs[0][0];
     }
 };
