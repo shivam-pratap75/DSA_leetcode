5e56c244c9ef741881fc5c6b8d4452809198fea3
shivam-pratap75 <shivam9193496575@gmail.com>
2026-02-01 20:33:28 +0530

    Time: 0 ms (100%), Space: 30.7 MB (17.09%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -4,25 +4,8 @@ public:
 
         int temp=nums[0];
 
-        sort(nums.begin(),nums.end());
-        int result=0;
-        bool check=false;
-        for(int i=0;i<=2;i++){
-            if(temp!=nums[i]){
-                result+=nums[i];
-            }else{
-                check=true;
-                result+=nums[i];
-            }
-        }
-        if(check){
-            return result;
-        }else{
-            result-=nums[2];
-            result+=temp;
-            return result;
-        }
-
+        sort(nums.begin()+1,nums.end());
+       return temp+nums[1]+nums[2];
         
     }
 };
