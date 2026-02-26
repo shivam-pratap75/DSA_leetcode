82016588ff6694b52aeec09bfd1ca8d88de11f73
shivam-pratap75 <shivam9193496575@gmail.com>
2026-02-27 00:18:01 +0530

    Time: 15 ms (90.7%), Space: 17.5 MB (23.25%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,42 +1,43 @@
 class Solution {
 public:
-    vector<vector<int >> fourSum(vector<int>& nums, int target) {
-
-    sort(nums.begin(),nums.end());
-    const int  n = nums.size();
-    int left,right;
-    vector<vector<int >> result;
-   
-   
-    for(int i=0;i<n-3;i++){
-    if(i>0 && nums[i]==nums[i-1] )continue;
-        for(int j=i+1;j<n-2;j++){
-            if(j>i+1&& nums[j]==nums[j-1] ) continue;
-            left=j+1;
-            right=n-1;
-
-            long long res=(long long)target-nums[i]-nums[j];
-            while(left<right){
-                long long sum=(long long)nums[left]+nums[right];
-             if(sum==res){
-                 result.push_back({nums[i],nums[j],nums[left],nums[right]});
-                while(left<right && nums[left]==nums[left+1])left++;
-              while(left<right && nums[right]==nums[right-1])right--;
-               right--;
-                left++;
-             }else if(sum<res){ 
-                left++;
-              }else{
-                 right--;
-              }         
-
-            }
-        }
-    }
-
-
-return result;
-
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
         
+         vector<vector<int>> result;
+         sort(nums.begin(),nums.end());
+        int n=nums.size();
+         for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1])continue;
+            for(int j=i+1;j<n;j++){
+                    if(j>i+1 && nums[j]==nums[j-1])continue;
+                    long long sum=nums[i]+nums[j];
+                    int left=j+1,right=n-1;
+                    while(left<right){
+                        long long check=nums[left]+nums[right]+sum;
+
+                        if(check==target){
+                            vector<int> temp;
+                            temp.push_back(nums[i]);
+                            temp.push_back(nums[j]);
+                            temp.push_back(nums[left]);
+                            temp.push_back(nums[right]);
+                            
+                                result.push_back(temp);
+                            while(left<right && nums[left]==nums[left+1])left++;
+                            while(left<right && nums[right]==nums[right-1])right--;
+                            
+                            
+                            left++;
+                            right--;
+
+                        }else if(check<target){
+                            left++;
+                        }else{
+                            right--;
+                        }
+                    }
+            }
+
+         }
+         return result;
     }
 };
